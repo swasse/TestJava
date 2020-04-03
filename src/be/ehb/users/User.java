0e4603316aad208def9443de546403db85345567
david.van.steertegem@gmail.com <Langwachtwoord1>
2020-04-03 15:02:51 +0200

    added User

---
+++ src/be/ehb/users/User.java
@@ -0,0 +1,17 @@
+package be.ehb.users;
+
+public class User {
+    private String naam;
+
+    public User(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+}
