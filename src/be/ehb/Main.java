b0dff1e88dbdfdf606380b9f3e5a38f332d8bb15
david.van.steertegem@gmail.com <Langwachtwoord1>
2020-04-03 14:47:20 +0200

    added calculations

--- src/be/ehb/Main.java
+++ src/be/ehb/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello world");
+        System.out.println("Hello world");
+        System.out.println(5 + 4);
     }
 }
