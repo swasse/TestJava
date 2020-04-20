33d55c3c4c6e7b27f2dcd9af17b685f81037c029
david.van.steertegem@gmail.com <Langwachtwoord1>
2020-04-20 09:44:02 +0200

    bugfixes

--- src/be/ehb/Main.java
+++ src/be/ehb/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+        System.out.println("Hello world Again");
         System.out.println(5 + 6);
     }
 }
